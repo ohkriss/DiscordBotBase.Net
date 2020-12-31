fc4292b0570c13891be50380286456de1bbe2fe7
Kriss <km.00@hotmail.com>
2020-12-31 08:06:21 +0100

    Create ReadyListener.cs

---
+++ Listeners/ReadyListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiscordBotBase.Logic.Event;
+using Discord;
+using Discord.WebSocket;
+using System.Diagnostics;
+
+namespace DiscordBotBase.Listeners
+{
+    public class ReadyListener
+    {
+        [DiscordEvent(EventType.Ready)]
+        public async Task ReadyAsync(BotShard shard, DiscordSocketClient client)
+        {
+            await client.SetGameAsync(
+                name: $"over {shard.Config.ShardCount} shard" + (shard.Config.ShardCount > 1 ? "s!" : "!"),
+                type: ActivityType.Watching
+                );
+
+            //await client.SetStatusAsync(UserStatus.Invisible);
+            Console.WriteLine($"Ready! >> {client.CurrentUser}");
+            Debug.WriteLine($"Ready! >> {client.CurrentUser}");
+        }
+    }
+}
